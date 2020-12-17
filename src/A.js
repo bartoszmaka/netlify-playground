9f39684c5b38a05d86a1dc034a678453272a32e5
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-12-17 12:55:50 +0100

    Add dummy components

---
+++ src/A.js
@@ -0,0 +1,1 @@
+export default function A() { return <div>A</div> }
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom';
+import A from './A';
+import B from './B';
+import C from './C';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ShowPath/>
+        <Switch>
+          <Route path="/A">
+            <A />
+          </Route>
+          <Route path="/B">
+            <B />
+          </Route>
+          <Route path="/C">
+            <C />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function ShowPath() {
+  const location = useLocation()
+  return (
+    <div>
+      <pre>
+        {JSON.stringify(location, null, 2)}
+      </pre>
+    </div>
+  )
+}
